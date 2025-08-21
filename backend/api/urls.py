d4fa1fbd55be2c87ea644f67a4a12ac8c14a4eef
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-08-20 20:20:28 -0700

    auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views.views import health, resume_review
 
 # views
 from .views.job_description import create_job_description, get_job_descriptions, delete_job_description
 from .views.background_tasks import test_task, get_task_status
+from .views.resume_analysis import resume_review
 
 urlpatterns = [
-    path("health/", health),
     path("resume-review/", resume_review),
     path("job-descriptions/", create_job_description),
     path("job-descriptions/all/", get_job_descriptions),
